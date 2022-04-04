ae8c447632d6193988cd401a5fd977a1c0a5b894
Clay Glad <rosquet@gmail.com>
2022-04-03 21:03:00 -0400

    Initial comit

---
+++ elpa/mozc-20210306.1053/mozc-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from mozc.el  -*- no-byte-compile: t -*-
+(define-package "mozc" "20210306.1053" "minor mode to input Japanese with Mozc" 'nil :commit "7329757e1ad30e327c1ae823a8302c79482d6b9c" :keywords '("mule" "multilingual" "input method"))
